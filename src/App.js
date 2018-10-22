a166404848f310ad219d23335da6b6b99178adcb
Berthelot Loïc <loic.berthelot1@gmail.com>
2018-10-22 13:03:55 +0200

    landing firstcommit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-
-        </header>
+      <div>
+      SAUL
       </div>
     );
   }
